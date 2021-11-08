47fc2eac047153311ae2d06621b1b15384e39024
nigerninja <50953629+nigerninja@users.noreply.github.com>
2021-11-08 21:48:10 +0100

    updated pipeline file

--- main.tf
+++ main.tf
@@ -16,6 +16,16 @@ provider "azurerm" {
     features {}
 }
 
+terraform {
+  backend "azurerm" {
+      resource_group_name   = "tf_rg_blobstore"
+      storage_account_name  = "tfstoragenigerninja"
+      container_name        = "tfstate"
+      key                   = "terraform.tfstate"
+  }
+  
+}
+
 resource "azurerm_resource_group" "tf_test" {
     name = "tfmainrg"
     location = "UK South"
